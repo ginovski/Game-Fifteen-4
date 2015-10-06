7d955af2ff01da318f58b1b7dac62452fb8f90b4
YordanGergov <yordan.gergov91@gmail.com>
2015-10-06 20:02:55 +0300

    Command abstract class
    Still thinking who will be his children

---
+++ Source/src/GameFifteen.ConsoleClient/Command.cs
@@ -0,0 +1,9 @@
+﻿namespace GameFifteen.ConsoleClient
+{   
+    /// <summary>Represents a command given by the user.</summary>
+    internal abstract class Command
+    {
+        /// <summary>Executes this object.</summary>
+        public abstract void Execute();
+    }
+}
