17c8099f7c19dd160e3d7c6e7ac0391bb8dfa62d
YordanGergov <yordan.gergov91@gmail.com>
2015-10-07 23:55:55 +0300

    Command pattern

---
+++ Source/src/GameFifteen.ConsoleClient/RestartCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFifteen.ConsoleClient
+{
+    class RestartCommand : Command
+    {
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
