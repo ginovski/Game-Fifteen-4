3af9266c50feab4195c62a50b17755da4d19c349
radenkovn <radenkovn@yahoo.com>
2015-10-12 11:13:40 +0300

    Documentation added for the BasicMatrix class

--- Source/src/GameFifteen.ConsoleClient/Matrixes/BasicMatrix.cs
+++ Source/src/GameFifteen.ConsoleClient/Matrixes/BasicMatrix.cs
@@ -4,19 +4,42 @@
     using System.Collections;
     using System.Linq;
 
+    /// <summary>
+    /// The base Matrix class
+    /// </summary>
     public abstract class BasicMatrix : IEnumerable
     {
+        /// <summary>
+        /// An array representing the direction rows
+        /// </summary>
         private int[] directionRow = { -1, 0, 1, 0 };
+
+        /// <summary>
+        /// An array representing the direction columns
+        /// </summary>
         private int[] directionColumn = { 0, 1, 0, -1 };
+
+        /// <summary>
+        /// The matrix of strings used in the program
+        /// </summary>
         private string[,] matrix;
 
+        /// <summary>
+        /// Initializes a new instance of the BasicMatrix class
+        /// </summary>
         public BasicMatrix()
         {
             this.EmptyCells = new int[2];
         }
 
+        /// <summary>
+        /// Gets or sets the array representing the empty cells
+        /// </summary>
         public int[] EmptyCells { get; set; }
 
+        /// <summary>
+        /// Gets or sets the property representing the direction rows
+        /// </summary>
         protected int[] DirectionRow
         {
             get
@@ -30,6 +53,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the property representing the direction columns
+        /// </summary>
         protected int[] DirectionColumn
         {
             get
@@ -43,6 +69,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the property representing the matrix
+        /// </summary>
         protected string[,] Matrix
         {
             get
@@ -56,8 +85,19 @@
             }
         }
 
+        /// <summary>
+        /// Represents an indexer
+        /// </summary>
+        /// <param name="row">Represents the rows</param>
+        /// <param name="column">Represents the columns</param>
+        /// <returns>The element in the matrix</returns>
         public abstract string this[int row, int column] { get; set; }
 
+        /// <summary>
+        /// Returns a boolean value representing if a cell is valid
+        /// </summary>
+        /// <param name="direction">Represents the direction</param>
+        /// <returns>Boolean value</returns>
         public bool IsNextCellValid(int direction)
         {
             var directions = this.GetDirections(direction);
@@ -70,6 +110,11 @@
             return isRowValid && isColumnValid;
         }
 
+        /// <summary>
+        /// Gets the direction array from a given direction
+        /// </summary>
+        /// <param name="direction">the direction</param>
+        /// <returns>Returns the direction array</returns>
         public int[] GetDirections(int direction)
         {
             var emptyCellRow = this.EmptyCells[0];
@@ -79,8 +124,15 @@
             return new int[] { nextCellRow, nextCellColumn };
         }
 
+        /// <summary>
+        /// Abstract class used to Initialize the matrix
+        /// </summary>
         public abstract void InitializeMatrix();
 
+        /// <summary>
+        /// Iterator Pattern
+        /// </summary>
+        /// <returns>Returns the values of the matrix</returns>
         public IEnumerator GetEnumerator()
         {
             for (int row = 0; row < Constants.GameBoardRows; row++)
